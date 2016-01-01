b84019816a3113d5bb5b029633486a281f5fec22
Juan Cruz Viotti <jviottidc@gmail.com>
2016-01-01 17:02:13 -0400

    Rename setProgress to setProgressState
    It shows the intention more clearly now that the function not only
    accepts a plain percentage.

--- build/browser/app.js
+++ build/browser/app.js
@@ -332,7 +332,7 @@ imageWriter.service('ImageWriterService', function($q, $timeout) {
   };
 
   /**
-   * @summary Set progress percentage
+   * @summary Set progress state
    * @function
    * @private
    *
@@ -340,11 +340,11 @@ imageWriter.service('ImageWriterService', function($q, $timeout) {
    * @param {Number} state.percentage - progress percentage
    *
    * @example
-   * ImageWriterService.setProgress({
+   * ImageWriterService.setProgressState({
    *   percentage: 50
    * });
    */
-  this.setProgress = function(state) {
+  this.setProgressState = function(state) {
 
     // Safely bring the state to the world of Angular
     $timeout(function() {
@@ -363,7 +363,7 @@ imageWriter.service('ImageWriterService', function($q, $timeout) {
    * ImageWriterService.reset();
    */
   this.reset = function() {
-    self.setProgress({
+    self.setProgressState({
       percentage: 0
     });
   };
@@ -452,7 +452,7 @@ imageWriter.service('ImageWriterService', function($q, $timeout) {
 
     self.setBurning(true);
 
-    return self.performWrite(image, drive, self.setProgress).finally(function() {
+    return self.performWrite(image, drive, self.setProgressState).finally(function() {
       self.setBurning(false);
     });
   };
